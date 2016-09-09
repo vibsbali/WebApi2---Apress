b20fd5aa80e9d8df04b0257f58b7a8567d982618
Vaibhav Bali <vibsbali@gmail.com>
2016-09-09 18:40:56 +1000

    Added a new Product Controller

---
+++ SportsStore/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using SportsStore.DAL;
+using SportsStore.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SportsStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public IRepository Repository { get; set; }
+
+        public ProductsController()
+        {
+            Repository = new Repository();
+        }
+
+        public IHttpActionResult GetProducts()
+        {
+            return Ok(Repository.Products);
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = Repository.Products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return BadRequest("No Product Found");
+            }
+
+            return Ok(product);
+        }
+
+        public async Task PostProduct(Product product)
+        {
+            await Repository.SaveProductAsync(product);
+        }
+
+        [Authorize(Roles = "Administrators")]
+        public async Task DeleteProduct(int id)
+        {
+            await Repository.DeleteProductAsync(id);
+
+        }
+    }
+}
