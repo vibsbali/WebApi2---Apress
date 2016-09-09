7f09e8ca1ec7be65c7734145127d33461fe3349f
Vaibhav Bali <vibsbali@gmail.com>
2016-09-09 19:25:21 +1000

    Added data validation attributes

---
+++ SportsStore/Migrations/201609090921184_DataValidation.cs
@@ -0,0 +1,22 @@
+namespace SportsStore.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataValidation : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Products", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "Description", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "Category", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "Category", c => c.String());
+            AlterColumn("dbo.Products", "Description", c => c.String());
+            AlterColumn("dbo.Products", "Name", c => c.String());
+        }
+    }
+}
